e0f11dfe09b43719fc3a518fb902c982b883602b
JH220 <admin@jh220.de>
2025-01-28 19:15:10 +0100

    Fix error when starting

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -5,7 +5,15 @@ module.exports = {
 	name: Events.GuildCreate,
 	once: true,
 	execute(guild : Guild) {
-		require('../utils/database').updateServer(guild);
-		(guild.client as CustomClient).debug(`[Guild ${guild.id}] Joined guild ${guild.name}.`);
+		const database = new (require('../utils/database'))();
+		const client = guild.client as CustomClient;
+
+		if (!database.getConnection()) {
+			client.error(`[Guild ${guild.id}] Failed to connect to the database.`);
+			return;
+		}
+
+		database.updateServer(guild);
+		client.debug(`[Guild ${guild.id}] Joined guild ${guild.name}.`);
 	},
 };
